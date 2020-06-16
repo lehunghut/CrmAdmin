76d85a42dc63db5f780d4931fda4c0d2e5dc88a4
HungLV <lehung.hut@gmail.com>
2020-06-16 23:07:44 +0700

    no message

---
+++ src/Installs/app/Models/Department.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Model generated using LaraAdmin
+ * Help: http://laraadmin.com
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Department extends Model
+{
+    use SoftDeletes;
+	
+	protected $table = 'departments';
+	
+	protected $hidden = [
+        
+    ];
+
+	protected $guarded = [];
+
+	protected $dates = ['deleted_at'];
+}
